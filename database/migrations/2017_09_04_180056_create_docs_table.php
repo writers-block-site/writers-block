7f5edb45faf5c0df2f20852d7d2471d00d81acb6
Daniel Rojo-Gutierrez <rojo@Daniels-MacBook-Pro.local>
2017-09-04 13:50:39 -0500

    created docs table migration

---
+++ database/migrations/2017_09_04_180056_create_docs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDocsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('docs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('uploaded_by')->unsigned();
+            $table->foreign('uploaded_by')->references('id')->on('users');
+            $table->integer('view_count');
+            $table->string('handles');
+            $table->string('title');
+            $table->string('genre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docs');
+    }
+}
