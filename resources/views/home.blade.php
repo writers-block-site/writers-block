4e8bc17e5c1397bcdc2a00158b6422e0dcecd19d
Douglass Hooker <DouglassHooker@gmail.com>
2017-09-03 23:38:43 -0500

    Establishing basics of home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,22 @@
 @extends('layouts._master')
 
+<?php
+    $test = 'Hello!';
+?>
+
 @section('content')
     <link rel='stylesheet' href='/css/app.css'></link>
     <div id='home'>
-        <div class='title'>
-            Hello World!
+        <div class='title-container'>
+            <span class='title' >Writer's Block <span class="glyphicon glyphicon-book book"></span></span>
+        </div>
+        <div>
+            <div class='container-fluid'> 
+                <span class='col-sm-4 text-center'>{{ $test }}</span>
+                <span class='col-sm-4 text-center'>{{ $test }}</span>
+                <span class='col-sm-4 text-center'>{{ $test }}</span>
+                
+            </div>
         </div>
     </div>
 @endsection
--- resources/views/layouts/_master.blade.php
+++ resources/views/layouts/_master.blade.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Writer's Block</title>
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel='stylesheet' href='/css/app.css'
 </head>
