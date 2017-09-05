92a29f16f81452a94d95497bbc6c0c2ae8f66fb5
Douglass Hooker <DouglassHooker@gmail.com>
2017-09-04 22:51:40 -0500

    Removed extra jQuery, dropdown works now

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,7 +76,6 @@
             @yield('content')
         </div>
     </div>
-
     <!-- Scripts -->
     <!-- jQuery library -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
