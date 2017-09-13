59928472a4a4f19a2f47c7a9a4794816e73515f3
Daniel Rojo-Gutierrez <rojo@Daniels-MacBook-Pro.local>
2017-09-13 10:53:17 -0500

    created seeders

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Models\Comments;
+use App\User;
+use App\Models\Docs;
 class CommentsTableSeeder extends Seeder
 {
     /**
@@ -11,6 +13,14 @@ class CommentsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+
+        for ($i=0; $i <=50 ; $i++) {
+            $comment = new Comments();
+            $comment->uploaded_by= User::all()->random()->id;
+            $comment->doc_id = Docs::all()->random()->id;
+            $comment->comment = $faker->bs;
+            $comment->save();
+        }
     }
 }
