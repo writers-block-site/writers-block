d0b512ecdfd26314f892156652e5c64f0715b941
Daniel Rojo-Gutierrez <rojo@Daniels-MacBook-Pro.local>
2017-09-14 10:33:24 -0500

    changed to properly represent 'handles'

--- resources/assets/js/components/views/PartialViews/components/iterators/UserDoc.js
+++ resources/assets/js/components/views/PartialViews/components/iterators/UserDoc.js
@@ -4,6 +4,7 @@ import {withRouter} from 'react-router-dom';
 class UserDoc extends Component {
     constructor(props){
         super(props);
+        // console.log(this.props.handle);
         // console.log('USERDOC', this.props)
     }
     openPost(){
@@ -23,17 +24,24 @@ class UserDoc extends Component {
 
             )
         }
+        var handlesArr = this.props.handle.split(',');
+        var links = handlesArr.map((link,index)=>{
+            return(
+                <li><a href={`http://cdn.filestackcontent.com/${link}`}>Version {index+1}</a></li>
+            )
+        })
+
         return(
                 <div className='col-md-4'>
                     <div
                     onClick={() => {
-                        this.openPost();
+                        // this.openPost();
                     }}
                     className=' user-document'>
                         <h3>{this.props.title}</h3>
                         <p>{this.props.type}</p>
                     </div>
-                    <button 
+                    <button
                     onClick={
                         () =>{
                             this.props.deletePost(this.props.id)
@@ -41,6 +49,13 @@ class UserDoc extends Component {
                     }
                     className='btn btn-danger'>Delete</button>
                     <a className='btn btn-warning' href={`/docs/${this.props.id}/edit`}>Edit Post</a>
+                    <div className="dropdown">
+                        <button className="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Download previous uploads
+                            <span className="caret"></span></button>
+                            <ul className="dropdown-menu">
+                                {links}
+                            </ul>
+                    </div>
                 </div>
 
             )
