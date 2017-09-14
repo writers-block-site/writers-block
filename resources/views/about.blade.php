9f367745cb09a39913c46ca7287957cd4abd4775
Douglass Hooker <DouglassHooker@gmail.com>
2017-09-14 15:20:24 -0500

    Challenging -> needs

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,7 +6,7 @@
     <h1 class='title'>Who We Are</h1>
     <div class="row">
         <p class='col-sm-offset-2 col-sm-8 body'>
-            We are a team of two passionate full stack developers dedicated and determined to finding solutions to your challenging
+            We are a team of two passionate full stack developers dedicated and determined to finding solutions to your needs
         </p>
 
     </div>
