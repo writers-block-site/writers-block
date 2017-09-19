e0f10b32d61962389b35fe419bae15feaaf0bc3d
Daniel Rojo-Gutierrez <rojo@Daniels-MacBook-Pro.local>
2017-09-19 14:48:30 -0500

    fliped info

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -13,7 +13,7 @@
 
             <div class="col-md-6">
 
-                    <img class="daniel" src="/css/img/DanielRojoGuttierrez1 (1).jpg" alt="">
+                    <img class="daniel" src="/css/img/DouglassHooker1.jpg" alt="">
 
                     <div class="row">
 
@@ -21,13 +21,13 @@
 
                         <div class="col-md-12">
 
-                            <a class="email"href="mailto:&#100;&#097;&#110;&#105;&#101;&#108;&#114;&#111;&#106;&#111;&#057;&#052;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">  &#100;&#097;&#110;&#105;&#101;&#108;&#114;&#111;&#106;&#111;&#057;&#052;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a>
+                            <a class="email"href="mailto:&#068;&#111;&#117;&#103;&#108;&#097;&#115;&#115;&#072;&#111;&#111;&#107;&#101;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">  &#068;&#111;&#117;&#103;&#108;&#097;&#115;&#115;&#072;&#111;&#111;&#107;&#101;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a>
                         </div>
 
                         <div class="col-md-12">
 
-                            <a class="email"href="http://github.com/duhanyul"><span>Github: Duhanyul</span></a>
 
+                            <a class="email"href="https://github.com/DukeFerdinand"><span>Github: DukeFerdinand</span></a>
                         </div>
 
                     </div>
@@ -36,20 +36,21 @@
 
             <div class="col-md-6">
 
-                    <img class="daniel" src="/css/img/DouglassHooker1.jpg" alt="">
+                    <img class="daniel" src="/css/img/DanielRojoGuttierrez1 (1).jpg" alt="">
 
                 <div class="row">
 
 
                         <div class="col-md-12">
 
-                            <a class="email"href="mailto:&#068;&#111;&#117;&#103;&#108;&#097;&#115;&#115;&#072;&#111;&#111;&#107;&#101;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">  &#068;&#111;&#117;&#103;&#108;&#097;&#115;&#115;&#072;&#111;&#111;&#107;&#101;&#114;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a>
+
+                            <a class="email"href="mailto:&#100;&#097;&#110;&#105;&#101;&#108;&#114;&#111;&#106;&#111;&#057;&#052;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;">  &#100;&#097;&#110;&#105;&#101;&#108;&#114;&#111;&#106;&#111;&#057;&#052;&#064;&#103;&#109;&#097;&#105;&#108;&#046;&#099;&#111;&#109;</a>
 
                         </div>
 
                         <div class="col-md-12">
+                            <a class="email"href="http://github.com/duhanyul"><span>Github: Duhanyul</span></a>
 
-                            <a class="email"href="https://github.com/DukeFerdinand"><span>Github: DukeFerdinand</span></a>
                         </div>
 
 
